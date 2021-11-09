5fa2690b93424f2e71f3aeb3cd50d2836131f02e
suhaiLap-hp <suhailm9055@gmail.com>
2021-11-09 11:34:57 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+
 import React, { Component } from 'react'
+import About from './Components/About/About'
+import Header from './Components/Home/Header'
 import TodoApp from './Components/TodoApp/TodoApp'
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+     <>
+        <Header/>
         <TodoApp/>
-      </div>
+        </>
     )
   }
 }
+
